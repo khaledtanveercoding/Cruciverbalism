283735c8d8b99ce184f6134d31b3a33ebdca74b0
Khaled Tanveer <khaledtanveer@hscs-mba-1753.lan>
2023-03-14 23:25:27 -0400

    new data

--- src/server/newGameManager.js
+++ src/server/newGameManager.js
@@ -1,12 +1,15 @@
 function getCrosswordData(date) {
   const [year, month, day] = date.split("-");
-  crossword = require(`./crosswords/${year}/${month}/${day}.json`); //save as a variable to feed into a new function
+  const crossword = require(`./crosswords/${year}/${month}/${day}.json`); //save as a variable to feed into a new function
   return crosswordMapper(crossword);
 }
 
 //bubbling up
 function crosswordMapper(crossword) {
-  //server stuff
+  //server stuff run through cell data
+  //cell data grid nums the clues 
+  console.log(crossword);
+  return crossword;
 }
 
 exports.getCrosswordData = getCrosswordData;
